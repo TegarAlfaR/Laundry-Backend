10fd9f4db3eff759dc006a56aa59524b8365f5c0
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-05-26 00:11:54 +0700

    fix: env name

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
