3fa81974abc6944b48b63ada89218d8bfc81d196
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-10-30 20:08:53 +0700

    feat: fix option in cors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
-const cors = require("cors");
 app.use(cookieParser());
 
 const allowedOrigins = [
@@ -23,7 +23,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
