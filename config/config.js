3f14e0d79b21996dad29e7b8cbe2cb58b7446ea3
TegarAlfaR <tegaralfa18052004@gmail.com>
2025-10-30 19:33:07 +0700

    fix: env

--- config/config.js
+++ config/config.js
@@ -3,13 +3,12 @@ dotenv.config();
 
 module.exports = {
   development: {
-    use_env_variable: "DATABASE_URL",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: process.env.DB_DEV_USER || "postgres",
+    password: process.env.DB_DEV_PASS || "postgres",
+    database: process.env.DB_DEV_NAME || "my_local_db",
+    host: process.env.DB_DEV_HOST || "127.0.0.1",
+    port: process.env.DB_DEV_PORT || 5432,
+    dialect: "postgres",
   },
   test: {
     username: "root",
